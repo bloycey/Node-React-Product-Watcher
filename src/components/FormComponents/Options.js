5122f38daac49d554c40c82eb2594dcbc051de34
Andrew <bloyce1990@gmail.com>
2018-11-23 23:57:57 +1000

    Added dummy toggles to Options panel

--- src/components/FormComponents/Options.js
+++ src/components/FormComponents/Options.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 import '../../App.css';
 
 class Options extends React.Component {
 
-state = {
-    tags: []
-}
+    state = {
+        checkedA: true,
+        checkedB: true,
+      };
+    
+      handleChange = name => event => {
+        this.setState({ [name]: event.target.checked });
+      };
+    
 
 render(){
 
 
     return (
-        <div>
-            <h1>OPTIONS GO HERE</h1>
-        </div>
+        <FormGroup row>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={this.state.checkedA}
+              onChange={this.handleChange('checkedA')}
+              value="checkedA"
+            />
+          }
+          label="Send me an email if the price of this product goes UP"
+        />
+        <br/>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={this.state.checkedB}
+              onChange={this.handleChange('checkedB')}
+              value="checkedB"
+              color="primary"
+            />
+          }
+          label="Send me an email if the price of this product goes DOWN"
+        />
+      </FormGroup>
     )
 }
 
